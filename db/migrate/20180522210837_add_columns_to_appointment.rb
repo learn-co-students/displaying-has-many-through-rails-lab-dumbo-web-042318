c24f0e06a8cd98d7893cbaa41af8ab4e290960fc
teasacura <teasacuraster@gmail.com>
2018-05-22 17:37:10 -0400

    Done.

---
+++ db/migrate/20180522210837_add_columns_to_appointment.rb
@@ -0,0 +1,6 @@
+class AddColumnsToAppointment < ActiveRecord::Migration
+  def change
+    add_column :appointments, :doctor_id, :integer
+    add_column :appointments, :patient_id, :integer
+  end
+end
